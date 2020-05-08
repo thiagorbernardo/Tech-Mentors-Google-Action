dcc12879e4b45d86df2509b4f9014d0a3fdf92ff
thiagorbernardo <thiagorbernardo@hotmail.com>
2020-05-08 14:15:48 -0300

    all functions from Alexa is working

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,10 @@
 const fetch = require('node-fetch');
 const API_KEYDB = '33b98784c91a88fdf6bf36da722e8ece'
 const APIUrl = 'https://claromentors.now.sh'
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+const prefixNames = ["st", "nd", "rd", "th"]
 // Import the Dialogflow module and response creation dependencies
 // from the Actions on Google client library.
 const {
@@ -59,11 +63,11 @@ app.intent('Movie Search', async (conv, { movieName }) => {
         // to embed an audio snippet in the response.
         conv.ask(`<speak>${conv.user.storage.userName}, ` +
             `${movie}.` +
-            ` Would you like to search a movie?</speak>`);
+            ` Would you like to search anything else?</speak>`);
         conv.ask(new Suggestions('Yes', 'No'));
     } else {
         conv.ask(`<speak>${movie}.` +
-            ` Would you like to search a movie?</speak>`);
+            ` Would you like to search anything else?</speak>`);
         conv.ask(new Suggestions('Yes', 'No'));
     }
 });
@@ -76,12 +80,48 @@ app.intent('Channel Search', async (conv, { channelName }) => {
         // If we collected user name previously, address them by name and use SSML
         // to embed an audio snippet in the response.
         conv.ask(`<speak>${conv.user.storage.userName}, ` +
-            `${channel}.` +
-            ` Would you like to search a channel Number?</speak>`);
+            `${channel}` +
+            ` Would you like to search anything else?</speak>`);
         conv.ask(new Suggestions('Yes', 'No'));
     } else {
-        conv.ask(`<speak>${channel}.` +
-            ` Would you like to search a channel Number?</speak>`);
+        conv.ask(`<speak>${channel}` +
+            ` Would you like to search anything else?</speak>`);
+        conv.ask(new Suggestions('Yes', 'No'));
+    }
+});
+
+// Handle the Dialogflow intent named 'Grade Search'.
+// The intent collects a parameter named 'channelName'.
+app.intent('Grade Search', async (conv, { channelName }) => {
+    const grade = await getChannelGradeNow(channelName)
+    if (conv.user.storage.userName) {
+        // If we collected user name previously, address them by name and use SSML
+        // to embed an audio snippet in the response.
+        conv.ask(`<speak>${conv.user.storage.userName}, ` +
+            `${grade}` +
+            ` Would you like to search anything else?</speak>`);
+        conv.ask(new Suggestions('Yes', 'No'));
+    } else {
+        conv.ask(`<speak>${grade}` +
+            ` Would you like to search anything else?</speak>`);
+        conv.ask(new Suggestions('Yes', 'No'));
+    }
+});
+
+// Handle the Dialogflow intent named 'New Exhibitions'.
+// The intent collects a parameter named 'channelName'.
+app.intent('New Exhibitions', async (conv, { channelName, title }) => {
+    const program = await getNewExhibitions(channelName, title)
+    if (conv.user.storage.userName) {
+        // If we collected user name previously, address them by name and use SSML
+        // to embed an audio snippet in the response.
+        conv.ask(`<speak>${conv.user.storage.userName}, ` +
+            `${program}` +
+            ` Would you like to search anything else?</speak>`);
+        conv.ask(new Suggestions('Yes', 'No'));
+    } else {
+        conv.ask(`<speak>${program}` +
+            ` Would you like to search anything else?</speak>`);
         conv.ask(new Suggestions('Yes', 'No'));
     }
 });
@@ -136,7 +176,13 @@ function setWordRequirements(word) {
     }
     return final.slice(0, final.length - 1);
 }
-
+function setTimeUSFormat(date){
+    let hours  = date.getUTCHours()
+    let AmOrPm = hours >= 12 ? 'pm' : 'am';
+    hours = (hours % 12) || 12;
+    let minutes = date.getUTCMinutes();
+    return (hours + ":" + minutes + " " + AmOrPm)
+}
 async function getChannelNumber(channelName) {
     let st_canal = channelName.toLowerCase()
     if (st_canal.length > 1)
@@ -159,5 +205,47 @@ async function getChannelNumber(channelName) {
     }
 }
 
+async function getChannelGradeNow(channelName) {
+    let st_canal = channelName.toLowerCase();
+    if (st_canal.length > 1)
+        st_canal = setWordRequirements(st_canal);
+    const url = `${APIUrl}/api/admin/channels/getChannelGradeNow?st_canal=${st_canal}&id_cidade=1`;
+    const response = await getData(url);
+
+    if (response != false) {
+        let title = response.titulo;
+        let final = new Date(response.dh_fim);
+        let hours = setTimeUSFormat(final)
+        return `${title} will be on ${channelName} until ${hours}.`
+    } else {
+        return "There was a problem, please try again."
+    }
+}
+
+async function getNewExhibitions(channelName, title){
+    let st_canal = channelName.toLowerCase();
+    if (st_canal.length > 1)
+        st_canal = setWordRequirements(st_canal);
+    
+    const url = `${APIUrl}/api/admin/channels/getNewExhibitions?st_canal=${st_canal}&title=${title}&id_cidade=1`
+    const response = await getData(url);
+    if(response != false){
+        let title = response.titulo;
+        let date_start = new Date(response.dh_inicio)
+        let date_end = new Date(response.dh_fim)
+        let time_start = setTimeUSFormat(date_start)
+        let time_end = setTimeUSFormat(date_end)
+        let month = date_start.getUTCMonth()
+        let day = response.dh_inicio.slice(8,10)
+        if(day <= 3){
+            day = day + prefixNames[day-1]
+        }else{
+            day = day + prefixNames[3]
+        }
+        return `${title} will be on ${channelName} again on ${monthNames[month]} ${day} from ${time_start} to ${time_end}.` 
+    } else {
+        return "There was a problem, please try again."
+    }
+}
 // Set the DialogflowApp object to handle the HTTPS POST request.
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
